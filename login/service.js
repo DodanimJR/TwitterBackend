7fce340746f4deff0d1f9d82f34031d421ccebc3
DodanimJr <dodanimjunior24@gmail.com>
2022-08-08 16:04:53 -0600

    j

--- login/service.js
+++ login/service.js
@@ -2,11 +2,11 @@ const { PrismaClient } = require('@prisma/client');
 var jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 dotenv.config();
-const secretKey = process.env.TOKEN_SECRET;
+const secretKey = "8ae350cc8b9ae710042d44353d7551f662a3b340a382e30b878670a5ec287a13e182d71aa9a1b93183b14bd742955b58b860f6c4b2b65c74a90d27bf683b5a50fae7344dbb004f3a954c00d689f1df0d4bdd5e4e339782759008591a893cfb7de8cb9b239de0b99815b2b68b129987a80893bbdea98e4ac6fb3a2b6fffaa68";
 const prisma = new PrismaClient()
 
 function generateAccessToken(username) {
-    return jwt.sign(username, process.env.TOKEN_SECRET, { expiresIn: '1800s' });
+    return jwt.sign(username, secretKey, { expiresIn: '1800s' });
   }
 
 const login = async(req,res)=>{
